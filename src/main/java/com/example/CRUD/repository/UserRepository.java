d21f952ae614c0280f7728cc535c6fbeb172d488
Sachin kumar <sachin.sankhla.ug22@nsut.ac.in>
2026-02-11 21:17:17 +0530

    adding search by name

--- src/main/java/com/example/CRUD/repository/UserRepository.java
+++ src/main/java/com/example/CRUD/repository/UserRepository.java
@@ -4,4 +4,5 @@ import com.example.CRUD.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepository extends JpaRepository<User,Long> {
+    List<User> findByNameContainingIgnoreCase(String name);
 }
